a525757e65036c611f06f6e440982c63406db129
kangseogin <kangk1739@daum.net>
2021-03-22 17:08:13 +0900

    제목: [docs] member html 추가, 수정 작성자: 강석인 본문:

--- ksmart_solar/src/main/java/com/cafe24/kangk0269/serivce/MemberService.java
+++ ksmart_solar/src/main/java/com/cafe24/kangk0269/serivce/MemberService.java
@@ -1,7 +1,9 @@
 package com.cafe24.kangk0269.serivce;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 @Service
 public class MemberService {
 
