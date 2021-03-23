e32e04c3eb1b7d18c939a146c726d0e4659870e0
ECS <ECS@DESKTOP-048HU3O>
2021-03-23 11:08:27 +0900

    제목: 	[label] 회원리스트 작성자: 	김지숙 본문: 	memberList

--- ksmart_solar/src/main/java/com/cafe24/kangk0269/controller/MemberManageController.java
+++ ksmart_solar/src/main/java/com/cafe24/kangk0269/controller/MemberManageController.java
@@ -1,14 +1,26 @@
 package com.cafe24.kangk0269.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.cafe24.kangk0269.dto.MemberDTO;
+import com.cafe24.kangk0269.serivce.MemberService;
 
 @Controller
 public class MemberManageController {
 
+	@Autowired
+	private MemberService memberService;
+	
 	@GetMapping("/member/memberList")
-	public String MemberList() {
-		
+	public String MemberList(Model model) {
+		List<MemberDTO> memberList = memberService.getAllMember();
+		System.out.println(memberList);
+		model.addAttribute("memberList", memberList);
 		return "/member/memberList";
 	}
 	
