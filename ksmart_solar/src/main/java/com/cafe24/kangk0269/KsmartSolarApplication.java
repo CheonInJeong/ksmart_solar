db7195addd1c29f5a0c07a60ed15b50b19e5280f
ECS <ECS@DESKTOP-048HU3O>
2021-03-24 14:39:00 +0900

    제목: 	[docs] ScheduledTimer, API 추가 작성자: 	강석인 본문: 	class 추가

--- ksmart_solar/src/main/java/com/cafe24/kangk0269/KsmartSolarApplication.java
+++ ksmart_solar/src/main/java/com/cafe24/kangk0269/KsmartSolarApplication.java
@@ -3,8 +3,10 @@ package com.cafe24.kangk0269;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.MultipartAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 //MultipartAutoConfiguration.class를 자동으로 구성하지 않도록 설정
 @SpringBootApplication(exclude= {MultipartAutoConfiguration.class})
+@EnableScheduling
 public class KsmartSolarApplication {
 
 	public static void main(String[] args) {
