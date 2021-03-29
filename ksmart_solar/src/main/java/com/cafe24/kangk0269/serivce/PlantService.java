82c3f36816d876577fdf7e1e2afc1b90fa882132
ECS <ECS@DESKTOP-048HU3O>
2021-03-29 11:42:52 +0900

    merge

--- ksmart_solar/src/main/java/com/cafe24/kangk0269/serivce/PlantService.java
+++ ksmart_solar/src/main/java/com/cafe24/kangk0269/serivce/PlantService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
 
 import com.cafe24.kangk0269.dao.PlantMapper;
 import com.cafe24.kangk0269.dto.BusinessPlantDTO;
@@ -27,6 +28,19 @@ public class PlantService {
 	
 	public List<BusinessPlantDTO> getPlantListById(String SID){
 		List<BusinessPlantDTO> plantList = plantMapper.getPlantListById(SID);
+		return plantList;
+	}
+
+	public List<BusinessPlantDTO> getPlantListByCode(Model model, String bzCode) {
+		List<BusinessPlantDTO> plantList = plantMapper.getPlantListByCode(bzCode);
+		
+		//발전량 예시(구현 예정)
+		double plantGenDay = 111.12;
+		double plantGenMonth = 36542.12;
+		
+		model.addAttribute("plantGenDay", plantGenDay);
+		model.addAttribute("plantGenMonth", plantGenMonth);
+		
 		return plantList;
 	}
 	
