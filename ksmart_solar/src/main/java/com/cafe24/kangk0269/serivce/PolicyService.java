cc8afdaa1009c825d8b42aafa94374a08e167c41
ECS <ECS@DESKTOP-048HU3O>
2021-04-05 17:22:58 +0900

    [features]정책 view 수정 중 [작성자] 천인정 [본문] 스케쥴러 설정 완료

--- ksmart_solar/src/main/java/com/cafe24/kangk0269/serivce/PolicyService.java
+++ ksmart_solar/src/main/java/com/cafe24/kangk0269/serivce/PolicyService.java
@@ -1,5 +1,7 @@
 package com.cafe24.kangk0269.serivce;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -14,6 +16,7 @@ import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import com.cafe24.kangk0269.common.FileUtils;
 import com.cafe24.kangk0269.dao.PolicyMapper;
+import com.cafe24.kangk0269.dao.SellMapper;
 import com.cafe24.kangk0269.dto.FileDTO;
 import com.cafe24.kangk0269.dto.StandardDTO;
 
@@ -32,6 +35,92 @@ public class PolicyService {
 		this.fileUtils = fileUtils;
 	}
 	
+	public void updateCommission() {
+		System.out.println("updateCommission 스케쥴러 실행");
+		List<StandardDTO> reservation = policyMapper.getCommissionReservationDate();
+		for(int i=0; i<reservation.size(); i++) {
+			String resrvationDate = reservation.get(i).getsReservation();
+			int type = reservation.get(i).getsCommissionType();
+			System.out.println("적용일 : "  +resrvationDate + "종류:" + type );
+			
+			if(resrvationDate!=null) {
+				Date date = new Date();
+				SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+				String today = format.format(date);
+				
+				if(resrvationDate.equals(today)) {
+					
+				
+					date = new Date(date.getTime()+(1000*60*60*24*-1));
+					String yesterday = format.format(date);
+					
+					//policyMapper.updateCommissionStatusN(type,yesterday);
+					policyMapper.updateCommissionStatusY(resrvationDate);
+				}
+			}
+		}
+	}
+	
+	//거래기간 정책 상태 바꾸기(스케쥴러)
+	public void updateTrade() {
+		System.out.println("updateTrade 스케쥴러 실행");
+		List<StandardDTO> reservation = policyMapper.getTradeReservationDate();
+		for(int i=0; i<reservation.size(); i++) {
+			String resrvationDate = reservation.get(i).getsReservation();
+			String type = reservation.get(i).getsTradeType();
+			System.out.println("적용일 : "  +resrvationDate + "종류:" + type );
+			
+			if(resrvationDate!=null) {
+				Date date = new Date();
+				SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+				String today = format.format(date);
+				
+				if(resrvationDate.equals(today)) {
+					
+					date = new Date(date.getTime()+(1000*60*60*24*-1));
+					String yesterday = format.format(date);
+					
+					StandardDTO standard = new StandardDTO();
+					standard.setsTradeLast(yesterday);
+					standard.setsTradeType(type);
+					
+					policyMapper.updateTradeStatusN(type,yesterday);
+					policyMapper.updateTradeStatusY(resrvationDate);
+				}
+			}
+		}
+	}
+	
+	//예치금 정책 상태 바꾸기(스케쥴러)
+	public void updateDeposit() {
+		System.out.println("updateDeposit 스케쥴러 실행");
+		List<StandardDTO> reservation = policyMapper.getDepositReservationDate();
+		for(int i=0 ; i<reservation.size();i++) {
+			String resrvationDate = reservation.get(i).getsReservation();
+			String type = reservation.get(i).getsDepositType();
+			System.out.println("적용일 : "  +resrvationDate + "종류:" + type );
+			if(resrvationDate!=null) {
+	
+				Date date = new Date();
+				SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+				String today = format.format(date);
+				if(resrvationDate.equals(today)) {
+					date = new Date(date.getTime()+(1000*60*60*24*-1));
+					String yesterday = format.format(date);
+					
+					StandardDTO standard = new StandardDTO();
+					standard.setsDepositLast(yesterday);
+					standard.setsDepositType(type);
+					
+					policyMapper.updateDepositStatusN(standard);
+					policyMapper.updateDepositStatusY(resrvationDate);
+				} 
+			}
+		}
+	}
+	
+	
+	
 	//파일삭제
 	public void  removeFile(int idx) {
 		 policyMapper.removeFile(idx);
@@ -41,8 +130,6 @@ public class PolicyService {
 	public FileDTO getFileInfo(int idx) {
 		return policyMapper.getFileInfo(idx);
 	}
-	
-
 	
 	
 	//업로드 된 입찰 파일 조회
@@ -65,7 +152,6 @@ public class PolicyService {
 			}
 		}
 	
-	
 	//공고 관련 파일 등록
 	public void addNoticeFile(MultipartHttpServletRequest multipartHttpServletRequest,HttpServletRequest request)throws Exception {
 		List<FileDTO> filelist = fileUtils.parseFileInfo("공고서류",1,"공고서류", multipartHttpServletRequest,request);
@@ -73,7 +159,6 @@ public class PolicyService {
 			policyMapper.addFile(filelist);
 		}
 	}
-	
 	
 	
 	public List<StandardDTO> getTradeHistory(String startDate, String endDate){
@@ -94,6 +179,9 @@ public class PolicyService {
 	
 	public void removeCommission(int idx) {
 		policyMapper.removeCommission(idx);
+	}
+	public void removeDeposit(int idx) {
+		policyMapper.removeDeposit(idx);
 	}
 	
 	
