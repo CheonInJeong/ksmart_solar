e069d6b8b3b62d81b375b3d73dacd9850ca0c775
Simjaehak <78127593+Simjaehak@users.noreply.github.com>
2021-04-09 17:44:15 +0900

    [bug] 페이징 거지같아 재학 페이징 개거지 같네

--- ksmart_solar/src/main/java/com/cafe24/kangk0269/common/Criteria.java
+++ ksmart_solar/src/main/java/com/cafe24/kangk0269/common/Criteria.java
@@ -3,8 +3,14 @@ package com.cafe24.kangk0269.common;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import com.cafe24.kangk0269.dto.BidComponentDTO;
+import com.cafe24.kangk0269.dto.BidPlantDTO;
+
 public class Criteria {
-	
+	private String currentPageNoName;
+	private String recordsPerPageName;
+	private String pageSizeName;
+	private int state;
 	/** 현재 페이지 번호 */
 	private int currentPageNo;
 
@@ -14,7 +20,6 @@ public class Criteria {
 	/** 화면 하단에 출력할 페이지 사이즈 */
 	private int pageSize;
 
-	
 	public int getCurrentPageNo() {
 		return currentPageNo;
 	}
@@ -39,8 +44,25 @@ public class Criteria {
 		this.pageSize = pageSize;
 	}
 
-	
-	
+	public String getCurrentPageNoName() {
+		return currentPageNoName;
+	}
+
+
+
+	public String getRecordsPerPageName() {
+		return recordsPerPageName;
+	}
+
+
+	public String getPageSizeName() {
+		return pageSizeName;
+	}
+
+
+	public int getState() {
+		return state;
+	}
 
 	@Override
 	public String toString() {
@@ -56,6 +78,22 @@ public class Criteria {
 	}
 
 	public Criteria() {
+		System.out.println(this instanceof BidComponentDTO);
+		System.out.println("<=========================부품");
+		if(this instanceof BidComponentDTO) {
+			this.state=2;
+			this.currentPageNoName = "currentPageNoCp";
+			this.recordsPerPageName = "recordsPerPageCp";
+			this.pageSizeName = "pageSizeCp";
+		}
+		System.out.println(this instanceof BidPlantDTO);
+		System.out.println("<=========================발전소");
+		if(this instanceof BidPlantDTO) {
+			this.currentPageNoName = "currentPageNoPl";
+			this.recordsPerPageName = "recordsPerPagePl";
+			this.pageSizeName = "pageSizePl";
+			state=1;
+		}
 		this.currentPageNo = 1;
 		this.recordsPerPage = 5;
 		this.pageSize = 5;
@@ -66,11 +104,12 @@ public class Criteria {
 	}
 	
 	public String makeQueryString(int pageNo) {
-
+		
 		UriComponents uriComponents = UriComponentsBuilder.newInstance()
-				.queryParam("currentPageNo", pageNo)
-				.queryParam("recordsPerPage", recordsPerPage)
-				.queryParam("pageSize", pageSize)
+				.queryParam(currentPageNoName, pageNo)
+				.queryParam(recordsPerPageName, recordsPerPage)
+				.queryParam(pageSizeName, pageSize)
+				.queryParam("state", state)
 				.build()
 				.encode();
 
