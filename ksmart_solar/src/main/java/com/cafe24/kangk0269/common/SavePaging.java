b23d35511d47719ba65337783c0054752bd54292
poiz1 <poiz1@121.186.43.139>
2021-04-11 18:30:19 +0900

    []세이브페이징 설명추가

--- ksmart_solar/src/main/java/com/cafe24/kangk0269/common/SavePaging.java
+++ ksmart_solar/src/main/java/com/cafe24/kangk0269/common/SavePaging.java
@@ -7,8 +7,13 @@ import javax.servlet.http.HttpSession;
 
 public class SavePaging {
 	private HttpSession session;
+	/** 현재 페이지 번호 */
 	private int currentPageNo;
+
+	/** 페이지당 출력할 데이터 개수 */
 	private int recordsPerPage;
+
+	/** 화면 하단에 출력할 페이지 사이즈 */
 	private int pageSize;
 	public SavePaging(int pagingNumber,HttpSession session) {
 		this.session = session;
@@ -20,6 +25,7 @@ public class SavePaging {
 			session.setAttribute(Integer.toString(i), pagingSave);
 		}
 	}
+	/** 세션에 이전화면의 현재 페이지 번호,페이지당 출력할 데이터 개수,화면 하단에 출력할 페이지 사이즈를 해당하는 리스트 맵객체의 변수에 저장  */
 	public void setPaging(int state,int currentPageNo, int recordsPerPage,int pageSize ) {
 		this.currentPageNo = currentPageNo;
 		this.recordsPerPage = recordsPerPage;
@@ -31,6 +37,7 @@ public class SavePaging {
 		savePaging.put("pageSize", this.pageSize);
 		session.setAttribute(Integer.toString(state), savePaging);
 	}
+	/** 세션에 이전화면의 현재 페이지 번호,페이지당 출력할 데이터 개수,화면 하단에 출력할 페이지 사이즈를 해당하는 리스트 맵객체의 변수를 불러옴  */
 	public void getPaging(Criteria criteria) {
 		@SuppressWarnings("unchecked")
 		Map<String, Object> Paging = (Map<String, Object>) session.getAttribute(Integer.toString(criteria.getState()));
