79c545f7e87b3ab6b30e9b210cfe3e65c8b8f357
ECS <ECS@DESKTOP-048HU3O>
2021-04-19 16:43:50 +0900

    [docs]file 업로드 [작성자] 천인정

--- ksmart_solar/src/main/java/com/cafe24/kangk0269/timer/ScheduledTimer.java
+++ ksmart_solar/src/main/java/com/cafe24/kangk0269/timer/ScheduledTimer.java
@@ -80,18 +80,16 @@ public class ScheduledTimer {
 			e.printStackTrace();
 		}
 	}
-/*
+	/*
 	  //테스트
 	  @Scheduled(cron = "0/5  *  *  *  *  *") 
 	  public void updateBidListStatustest() throws IOException, ParseException, ClassNotFoundException, SQLException {
 		  try { 
-			  scheduledService.updatePriority();
 		  } catch (Exception e) { 
 			  e.printStackTrace(); 
 		  } 
 	  }
-*/	
-
+	 */
 	
 	//예약한 시간에 정책 적용 상태 바꾸는 메서드
 	@Scheduled(cron = "30  00  0  *  *  *") 
