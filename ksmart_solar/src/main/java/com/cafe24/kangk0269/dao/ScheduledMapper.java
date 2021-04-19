b9855ed044ff82155d8c2662d417282e73b45d27
Simjaehak <78127593+Simjaehak@users.noreply.github.com>
2021-04-19 13:39:15 +0900

    [docs] 계약 취소시 2순위 등록 재학 계약 취소시 2순위 등록 진행중

--- ksmart_solar/src/main/java/com/cafe24/kangk0269/dao/ScheduledMapper.java
+++ ksmart_solar/src/main/java/com/cafe24/kangk0269/dao/ScheduledMapper.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
+import com.cafe24.kangk0269.dto.BidListDTO;
 import com.cafe24.kangk0269.dto.TradePriorityDTO;
 
 @Mapper
@@ -14,5 +15,10 @@ public interface ScheduledMapper {
 						@Param(value="tomorrow") String tomorrow);
 	
 	public List<TradePriorityDTO> getPriority();
+	//계약이 취소된 낙찰자의 공고코드와 순위가져오는 쿼리
+	public List<TradePriorityDTO> getPriorityFail();
+	//낙찰자 테이블에서 취소가 되면 다음순위 낙찰자 가져오는 쿼리
+	public String getBidListNextRank(@Param(value="announcedCode") String announcedCode,
+										 @Param(value="rank") int rank);
 	
 }
--- ksmart_solar/src/main/java/com/cafe24/kangk0269/serivce/ScheduledService.java
+++ ksmart_solar/src/main/java/com/cafe24/kangk0269/serivce/ScheduledService.java
@@ -4,15 +4,24 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.cafe24.kangk0269.common.FileUtils;
+import com.cafe24.kangk0269.dao.BidComponentMapper;
+import com.cafe24.kangk0269.dao.BidPlantMapper;
 import com.cafe24.kangk0269.dao.FileMapper;
+import com.cafe24.kangk0269.dao.PolicyMapper;
 import com.cafe24.kangk0269.dao.ScheduledMapper;
 import com.cafe24.kangk0269.dao.SellMapper;
+import com.cafe24.kangk0269.dao.TradeMapper;
+import com.cafe24.kangk0269.dto.BidComponentDTO;
+import com.cafe24.kangk0269.dto.BidListDTO;
+import com.cafe24.kangk0269.dto.BidPlantDTO;
 import com.cafe24.kangk0269.dto.TradePriorityDTO;
 
 
@@ -20,6 +29,14 @@ import com.cafe24.kangk0269.dto.TradePriorityDTO;
 public class ScheduledService {
 	@Autowired
 	private final ScheduledMapper scheduledMapper;
+	@Autowired
+	private BidComponentMapper bidComponentMapper;
+	@Autowired
+	private BidPlantMapper bidPlantMapper;
+	@Autowired
+	private TradeMapper tradeMapper;
+	@Autowired
+	private PolicyMapper policyMapper;
 
 	
 	public ScheduledService(ScheduledMapper scheduledMapper) {
@@ -52,13 +69,37 @@ public class ScheduledService {
 				
 				}
 			}
-				
-				
-				
-
-				
+	
 		}
 		
 	}
-	
+	public void updatePriority() {
+		List<TradePriorityDTO> priorityFail = scheduledMapper.getPriorityFail();
+		int tradePerioddate = policyMapper.getTradePeriod();
+		for(int i=0; i<priorityFail.size(); i++) {
+			String announcedCode = priorityFail.get(i).getAnnouncedCode();
+			int rank = priorityFail.get(i).getTrPrRank();
+			rank++;
+			String bidListDTO = scheduledMapper.getBidListNextRank(announcedCode, rank);
+			System.out.println(bidListDTO);
+			if(bidListDTO!=null) {
+				if(bidListDTO.equals("1")) {
+					System.out.println("발전소");
+					BidPlantDTO BidPlantTradeList = bidPlantMapper.getBidPlantTradeNext(announcedCode, rank);
+					Map<String,Object> List = new HashMap<String, Object>();
+					List.put("tradePerioddate", tradePerioddate);
+					List.put("BidPlantTradeList", BidPlantTradeList);
+					tradeMapper.addTradePriority(List);
+				}
+				if(bidListDTO.equals("2")) {
+					System.out.println("부품");
+					BidComponentDTO BidComTradeList = bidComponentMapper.getBidComTradeNext(announcedCode, rank);
+					Map<String,Object> List = new HashMap<String, Object>();
+					List.put("tradePerioddate", tradePerioddate);
+					List.put("BidComTradeList", BidComTradeList);
+					tradeMapper.addTradePriority(List);
+				}
+			}
+		}
+	}
 }
